cb51587c07df260828494823191872736fbd473c
Michael Mahgu <mahgu.blender@gmail.com>
2023-02-08 00:10:19 -0500

    small refactor

--- shared-texture-filter.cpp
+++ shared-texture-filter.cpp
@@ -1,138 +1,106 @@
+#include "shared-texture-filter.h"
+
 #include <string>
-#include <obs-module.h>
-#include <graphics/graphics.h>
-
-#include <d3d11.h>
-#include <dxgi.h>
-#include <wrl/client.h>
-
-#define OBS_SHARED_TEXTURE_PLUGIN "shared-texture-filter"
-#define OBS_SHARED_TEXTURE_VERSION_MAJOR 0
-#define OBS_SHARED_TEXTURE_VERSION_MINOR 0
-#define OBS_SHARED_TEXTURE_VERSION_RELEASE 1
-#define OBS_SHARED_TEXTURE_VERSION_STRING "0.0.1"
-#define OBS_SHARED_TEXTURE_LANG "en-US"
-#define OBS_SHARED_TEXTURE_COLOR_SPACE GS_BGRA
 
 OBS_DECLARE_MODULE()
-OBS_MODULE_USE_DEFAULT_LOCALE(OBS_SHARED_TEXTURE_PLUGIN, OBS_SHARED_TEXTURE_LANG)
+OBS_MODULE_USE_DEFAULT_LOCALE(OBS_PLUGIN, OBS_PLUGIN_LANG)
 
-#define do_log(level, format, ...) blog(level, "[shared-texture-filter] " format, ##__VA_ARGS__)
-
-#define error(format, ...) do_log(LOG_ERROR, format, ##__VA_ARGS__)
-#define warn(format, ...) do_log(LOG_WARNING, format, ##__VA_ARGS__)
-#define info(format, ...) do_log(LOG_INFO, format, ##__VA_ARGS__)
-#define debug(format, ...) do_log(LOG_DEBUG, format, ##__VA_ARGS__)
-
-#define OBS_UI_SETTING_FILTER_NAME "mahgu.sharedtexture.ui.filter_title"
-#define OBS_UI_SETTING_DESC_NAME "mahgu.sharedtexture.ui.name_desc"
-
-struct shared_texture_filter {
-	obs_source_t *context;
-
-	gs_texrender_t *texrender_current;
-	gs_texrender_t *texrender_previous;
-	
-	gs_texture_t *texture_shared;
-
-	uint32_t shared_width;
-	uint32_t shared_height;
-
-	Microsoft::WRL::ComPtr<ID3D11DeviceContext> d3d11_contextPtr;
-
-	ID3D11Texture2D *d3d11_texture_shared_ptr;
-	ID3D11Texture2D *d3d11_texture_current_ptr;
-	ID3D11Texture2D *d3d11_texture_previous_ptr;
-
-	bool render_swap;
-	bool render_flush;
-};
-
-static const char *shared_texture_filter_get_name(void *unused)
+static const char *filter_get_name(void *unused)
 {
-	UNUSED_PARAMETER(unused);	
+	UNUSED_PARAMETER(unused);
 	return obs_module_text(OBS_UI_SETTING_FILTER_NAME);
 }
 
-static obs_properties_t *shared_texture_filter_properties(void *data)
+static obs_properties_t *filter_properties(void *data)
 {
 	UNUSED_PARAMETER(data);
 
 	auto props = obs_properties_create();
 
-	obs_properties_add_text(props, OBS_UI_SETTING_DESC_NAME, obs_module_text(OBS_UI_SETTING_DESC_NAME), OBS_TEXT_DEFAULT);
+	obs_properties_add_text(props, OBS_UI_SETTING_DESC_NAME,
+				obs_module_text(OBS_UI_SETTING_DESC_NAME),
+				OBS_TEXT_DEFAULT);
 
 	return props;
 }
 
-static void shared_texture_filter_defaults(obs_data_t *settings)
+static void filter_defaults(obs_data_t *settings)
 {
 	UNUSED_PARAMETER(settings);
 }
 
-static void shared_texture_filter_initialize_texrender(void* data, uint32_t width, uint32_t height)
-{
-	auto filter = (struct shared_texture_filter *)data;
-	
-	gs_texrender_reset(filter->texrender_current);
-	if (gs_texrender_begin(filter->texrender_current,width,height)) {
-		gs_texrender_end(filter->texrender_current);
-	}	
+namespace SharedTexture {
 
-	gs_texrender_reset(filter->texrender_previous);
-	if (gs_texrender_begin(filter->texrender_previous, width, height)) {
-		gs_texrender_end(filter->texrender_previous);
-	}	
+static void initialize_texrender(void *data, uint32_t width, uint32_t height)
+{
+	auto filter = (struct filter *)data;
+
+	gs_texrender_reset(filter->texrender_current_ptr);
+	if (gs_texrender_begin(filter->texrender_current_ptr, width, height)) {
+		gs_texrender_end(filter->texrender_current_ptr);
+	}
+
+	gs_texrender_reset(filter->texrender_previous_ptr);
+	if (gs_texrender_begin(filter->texrender_previous_ptr, width, height)) {
+		gs_texrender_end(filter->texrender_previous_ptr);
+	}
 }
 
-static void shared_texture_filter_update_texture_pointers(void *data)
-{
-	auto filter = (struct shared_texture_filter *)data;	
 
-	filter->d3d11_texture_current_ptr = nullptr;
-	filter->d3d11_texture_current_ptr =
+static void update_texture_pointers(void *data)
+{
+	auto filter = (struct filter *)data;
+
+	filter->d3d11_current_ptr = nullptr;
+	filter->d3d11_current_ptr =
 		(ID3D11Texture2D*)gs_texture_get_obj(
-		gs_texrender_get_texture(filter->texrender_current));
+		gs_texrender_get_texture(filter->texrender_current_ptr));
 
-	filter->d3d11_texture_previous_ptr = nullptr;
-	filter->d3d11_texture_previous_ptr =
+	filter->d3d11_previous_ptr = nullptr;
+	filter->d3d11_previous_ptr =
 		(ID3D11Texture2D *)gs_texture_get_obj(
-		gs_texrender_get_texture(filter->texrender_previous));
+		gs_texrender_get_texture(filter->texrender_previous_ptr));
 
-	filter->d3d11_texture_shared_ptr = nullptr;
-	filter->d3d11_texture_shared_ptr =
-		(ID3D11Texture2D *)gs_texture_get_obj(filter->texture_shared);
+	filter->d3d11_shared_ptr = nullptr;
+	filter->d3d11_shared_ptr =
+		(ID3D11Texture2D *)gs_texture_get_obj(filter->texture_shared_ptr);
 }
 
-static void shared_texture_filter_create_d3d11_context(void* data)
+static void create_d3d11_context(void* data)
 {
-	auto filter = (struct shared_texture_filter *)data;
+	auto filter = (struct filter *)data;
 	Microsoft::WRL::ComPtr<ID3D11Device> d3d11_device = (ID3D11Device *)gs_get_device_obj();
-	d3d11_device->GetImmediateContext(&filter->d3d11_contextPtr);
+	d3d11_device->GetImmediateContext(&filter->d3d11_context_ptr);
 	d3d11_device = nullptr;
 }
 
-static void shared_texture_filter_create_shared_texture(void* data, uint32_t cx, uint32_t cy)
+static void create_shared_texture(void* data, uint32_t cx, uint32_t cy)
 {
-	auto filter = (struct shared_texture_filter *)data;
-	filter->texture_shared = gs_texture_create(cx, cy, OBS_SHARED_TEXTURE_COLOR_SPACE, 1, NULL, GS_SHARED_TEX);
+	auto filter = (struct filter *)data;
+	if (filter->texture_shared_ptr) {
+		// should not be here
+		warn("create_shared_texture warning :: shared texture not empty");
+		// we should already be inside graphics
+		gs_texture_destroy(filter->texture_shared_ptr);
+	}
+	filter->texture_shared_ptr = gs_texture_create(cx, cy, OBS_PLUGIN_COLOR_SPACE, 1, NULL, GS_SHARED_TEX);
 }
 
-static void shared_texture_filter_update_shared_texture_handle(void* data)
+static void update_shared_texture_handle(void* data)
 {
-	auto filter = (struct shared_texture_filter *)data;
-	auto handle = gs_texture_get_shared_handle(filter->texture_shared);
-	auto ws = "\r\n\r\n\r\nSHARED TEXTURE HANDLE : " + std::to_string(handle) + "\r\n\r\n\r\n";
+	auto filter = (struct filter *)data;
+	auto handle = gs_texture_get_shared_handle(filter->texture_shared_ptr);
+	auto ws = "\r\n\r\n\r\n<<<===>>> SHARED TEXTURE HANDLE : " + std::to_string(handle) + "\r\n\r\n\r\n";
 	blog(LOG_INFO, ws.c_str());
 }
 
-static void shared_texture_fitler_render_texture(void *data, obs_source_t *target, uint32_t cx, uint32_t cy)
+static void render_texture(void *data, obs_source_t *target, uint32_t cx, uint32_t cy)
 {
-	auto filter = (struct shared_texture_filter *)data;	
+	auto filter = (struct filter *)data;	
 
 	gs_texrender_t *buffer_texrender = filter->render_swap
-						   ? filter->texrender_current
-						   : filter->texrender_previous;
+						   ? filter->texrender_current_ptr
+						   : filter->texrender_previous_ptr;
 
 	gs_texrender_reset(buffer_texrender);
 	if (gs_texrender_begin(buffer_texrender, cx, cy)) {
@@ -154,28 +122,28 @@ static void shared_texture_fitler_render_texture(void *data, obs_source_t *targe
 	buffer_texrender = nullptr;
 }
 
-static void shared_texture_filter_copy_texture_resources(void* data)
+static void copy_texture_resources(void* data)
 {
-	auto filter = (struct shared_texture_filter *)data;
+	auto filter = (struct filter *)data;
 
-	if (filter->d3d11_contextPtr) {
+	if (filter->d3d11_context_ptr) {
 
 		if (filter->render_swap) {
-			filter->d3d11_contextPtr->CopyResource(
-				filter->d3d11_texture_shared_ptr,
-				filter->d3d11_texture_current_ptr);
+			filter->d3d11_context_ptr->CopyResource(
+				filter->d3d11_shared_ptr,
+				filter->d3d11_current_ptr);
 		} else {
-			filter->d3d11_contextPtr->CopyResource(
-				filter->d3d11_texture_shared_ptr,
-				filter->d3d11_texture_previous_ptr);
+			filter->d3d11_context_ptr->CopyResource(
+				filter->d3d11_shared_ptr,
+				filter->d3d11_previous_ptr);
 		}
 
 		if (filter->render_flush)
-			filter->d3d11_contextPtr->Flush();
+			filter->d3d11_context_ptr->Flush();
 	}
 }
 
-static void shared_texture_filter_render_callback(void *data, uint32_t cx, uint32_t cy)
+static void filter_render_callback(void *data, uint32_t cx, uint32_t cy)
 {
 	UNUSED_PARAMETER(cx);
 	UNUSED_PARAMETER(cy);
@@ -183,7 +151,7 @@ static void shared_texture_filter_render_callback(void *data, uint32_t cx, uint3
 	if (!data)
 		return;
 
-	auto filter = (struct shared_texture_filter *)data;
+	auto filter = (struct filter *)data;
 
 	if (!filter->context)
 		return;
@@ -196,15 +164,15 @@ static void shared_texture_filter_render_callback(void *data, uint32_t cx, uint3
 	auto target_width = obs_source_get_base_width(target);
 	auto target_height = obs_source_get_base_height(target);
 
-	auto size_changed = filter->shared_width != target_width ||
-			    filter->shared_height != target_height;
+	auto size_changed = filter->texture_shared_width != target_width ||
+			    filter->texture_shared_height != target_height;
 
 	// update shared sizes if changed
-	if (filter->shared_width != target_width) {
-		filter->shared_width = target_width;
+	if (filter->texture_shared_width != target_width) {
+		filter->texture_shared_width = target_width;
 	}
-	if (filter->shared_height != target_height) {
-		filter->shared_height = target_height;
+	if (filter->texture_shared_height != target_height) {
+		filter->texture_shared_height = target_height;
 	}
 	
 	if (target_width == 0 || target_height == 0)
@@ -212,91 +180,89 @@ static void shared_texture_filter_render_callback(void *data, uint32_t cx, uint3
 
 	if (size_changed)
 	{
-		if (filter->texture_shared)
-		{
-			blog(LOG_INFO, "\r\n\r\nTEXTURE DESTROYED\r\n\r\n");
-			gs_texture_destroy(filter->texture_shared);
-			filter->texture_shared = nullptr;
+		if (filter->texture_shared_ptr)
+		{			
+			gs_texture_destroy(filter->texture_shared_ptr);
+			filter->texture_shared_ptr = nullptr;
 		}
 	}
 
 	// create shared texture
-	if (!filter->texture_shared)
+	if (!filter->texture_shared_ptr)
 	{		
-		shared_texture_filter_create_shared_texture(filter, target_width, target_height);
-		shared_texture_filter_update_shared_texture_handle(filter);
+		create_shared_texture(filter, target_width, target_height);
+		update_shared_texture_handle(filter);
 		
-		if (!filter->d3d11_contextPtr)
-			shared_texture_filter_create_d3d11_context(filter);
+		if (!filter->d3d11_context_ptr)
+			create_d3d11_context(filter);
 		
-		shared_texture_filter_initialize_texrender(filter, target_width, target_height);
-		shared_texture_filter_update_texture_pointers(filter);
+		initialize_texrender(filter, target_width, target_height);
+		update_texture_pointers(filter);
 
 		return;
 	}
 	
-	shared_texture_fitler_render_texture(filter, target, target_width, target_height);
-	shared_texture_filter_copy_texture_resources(filter);
+	render_texture(filter, target, target_width, target_height);
+	copy_texture_resources(filter);
 		
 	filter->render_swap = !filter->render_swap;
 }
 
-static void shared_texture_filter_update(void *data, obs_data_t *settings)
+static void filter_update(void *data, obs_data_t *settings)
 {
 	UNUSED_PARAMETER(settings);
 
-	auto filter = (struct shared_texture_filter *)data;
+	auto filter = (struct filter *)data;
 
-	obs_remove_main_render_callback(shared_texture_filter_render_callback, filter);
+	obs_remove_main_render_callback(filter_render_callback, filter);
 
 	// do some thing??
 
-	obs_add_main_render_callback(shared_texture_filter_render_callback, filter);	
+	obs_add_main_render_callback(filter_render_callback, filter);	
 }
 
-static void *shared_texture_filter_create(obs_data_t *settings, obs_source_t *source)
+static void *filter_create(obs_data_t *settings, obs_source_t *source)
 {	
-	auto filter = (struct shared_texture_filter *)bzalloc(sizeof(shared_texture_filter));
+	auto filter = (struct filter *)bzalloc(sizeof(SharedTexture::filter));
 
 	filter->context = source;
 
 	filter->render_flush = true;
 		
-	filter->texrender_previous =
-		gs_texrender_create(OBS_SHARED_TEXTURE_COLOR_SPACE, GS_ZS_NONE);
+	filter->texrender_previous_ptr =
+		gs_texrender_create(OBS_PLUGIN_COLOR_SPACE, GS_ZS_NONE);
 
-	filter->texrender_current =
-		gs_texrender_create(OBS_SHARED_TEXTURE_COLOR_SPACE, GS_ZS_NONE);
-		
-	shared_texture_filter_update(filter, settings);	
+	filter->texrender_current_ptr =
+		gs_texrender_create(OBS_PLUGIN_COLOR_SPACE, GS_ZS_NONE);
+
+	// force an update
+	filter_update(filter, settings);
 
 	return filter;
 }
 
-static void shared_texture_filter_destroy(void *data)
+static void filter_destroy(void *data)
 {
-	auto filter = (struct shared_texture_filter *)data;	
+	auto filter = (struct filter *)data;	
 	
 	if (filter)
 	{
-
-		obs_remove_main_render_callback(
-			shared_texture_filter_render_callback, filter);
+		obs_remove_main_render_callback(filter_render_callback, filter);
 
 		obs_enter_graphics();
 
-		filter->d3d11_texture_shared_ptr = nullptr;
-		filter->d3d11_texture_current_ptr = nullptr;
-		filter->d3d11_texture_previous_ptr = nullptr;
+		filter->d3d11_shared_ptr = nullptr;
+		filter->d3d11_current_ptr = nullptr;
+		filter->d3d11_previous_ptr = nullptr;
 
-		filter->d3d11_contextPtr = nullptr;
+		filter->d3d11_context_ptr = nullptr;
 
-		gs_texrender_destroy(filter->texrender_current);		
-		gs_texrender_destroy(filter->texrender_previous);
+		gs_texrender_destroy(filter->texrender_current_ptr);		
+		gs_texrender_destroy(filter->texrender_previous_ptr);
 
-		if (filter->texture_shared)
+		if (filter->texture_shared_ptr)
 		{
-			gs_texture_destroy(filter->texture_shared);
+			gs_texture_destroy(filter->texture_shared_ptr);
 		}
 
 		obs_leave_graphics();
@@ -304,11 +270,11 @@ static void shared_texture_filter_destroy(void *data)
 	}
 }
 
-static void shared_texture_filter_video_render(void* data, gs_effect_t* effect)
+static void filter_video_render(void* data, gs_effect_t* effect)
 {	
 	UNUSED_PARAMETER(effect);
 
-	auto* filter = (struct shared_texture_filter *)data;
+	auto* filter = (struct filter *)data;
 
 	if (!filter->context)
 		return;
@@ -316,38 +282,16 @@ static void shared_texture_filter_video_render(void* data, gs_effect_t* effect)
 	obs_source_skip_video_filter(filter->context);
 }
 
-struct obs_source_info create_filter_info()
-{
-	struct obs_source_info shared_texture_filter_info = {};
-
-	shared_texture_filter_info.id = "shared_texture_filter";
-
-	shared_texture_filter_info.type = OBS_SOURCE_TYPE_FILTER;	
-	shared_texture_filter_info.output_flags = OBS_SOURCE_VIDEO | OBS_SOURCE_SRGB;
-
-	shared_texture_filter_info.get_name = shared_texture_filter_get_name;
-
-	shared_texture_filter_info.get_properties = shared_texture_filter_properties;
-	shared_texture_filter_info.get_defaults = shared_texture_filter_defaults;
-
-	shared_texture_filter_info.create = shared_texture_filter_create;
-	shared_texture_filter_info.destroy = shared_texture_filter_destroy;
-		
-	shared_texture_filter_info.video_render = shared_texture_filter_video_render;
-
-	shared_texture_filter_info.update = shared_texture_filter_update;
-
-	return shared_texture_filter_info;
-}
+} // namespace SharedTexture
 
 static void debug_report_version()
 {
-	info("you can haz shared-texture tooz (Version: %s)", OBS_SHARED_TEXTURE_VERSION_STRING);	
+	info("you can haz shared-texture tooz (Version: %s)", OBS_PLUGIN_VERSION_STRING);	
 }
 
 bool obs_module_load(void)
 {
-	auto filter_info = create_filter_info();
+	auto filter_info = SharedTexture::create_filter_info();
 
 	obs_register_source(&filter_info);
 	
---
+++ shared-texture-filter.h
@@ -0,0 +1,98 @@
+#pragma once
+
+#include <obs-module.h>
+#include <graphics/graphics.h>
+
+#include <d3d11.h>
+#include <dxgi.h>
+#include <wrl/client.h>
+
+#define OBS_PLUGIN "shared-texture-filter"
+#define OBS_PLUGIN_ "shared_texture_filter"
+#define OBS_PLUGIN_VERSION_MAJOR 0
+#define OBS_PLUGIN_VERSION_MINOR 0
+#define OBS_PLUGIN_VERSION_RELEASE 1
+#define OBS_PLUGIN_VERSION_STRING "0.0.1"
+#define OBS_PLUGIN_LANG "en-US"
+#define OBS_PLUGIN_COLOR_SPACE GS_BGRA
+
+#define OBS_UI_SETTING_FILTER_NAME "mahgu.sharedtexture.ui.filter_title"
+#define OBS_UI_SETTING_DESC_NAME "mahgu.sharedtexture.ui.name_desc"
+
+#define obs_log(level, format, ...) \
+	blog(level, "[shared-texture-filter] " format, ##__VA_ARGS__)
+
+#define error(format, ...) obs_log(LOG_ERROR, format, ##__VA_ARGS__)
+#define warn(format, ...) obs_log(LOG_WARNING, format, ##__VA_ARGS__)
+#define info(format, ...) obs_log(LOG_INFO, format, ##__VA_ARGS__)
+#define debug(format, ...) obs_log(LOG_DEBUG, format, ##__VA_ARGS__)
+
+static const char *filter_get_name(void *unused);
+static obs_properties_t *filter_properties(void *data);
+static void filter_defaults(obs_data_t *settings);
+
+bool obs_module_load(void);
+void obs_module_unload();
+
+static void debug_report_version();
+
+namespace SharedTexture {
+
+static void initialize_texrender(void *data, uint32_t width, uint32_t height);
+static void update_texture_pointers(void *data);
+static void create_d3d11_context(void *data);
+static void create_shared_texture(void *data, uint32_t cx, uint32_t cy);
+static void update_shared_texture_handle(void *data);
+static void render_texture(void *data, obs_source_t *target, uint32_t cx, uint32_t cy);
+static void copy_texture_resources(void *data);
+static void filter_render_callback(void *data, uint32_t cx, uint32_t cy);
+static void filter_update(void *data, obs_data_t *settings);
+static void *filter_create(obs_data_t *settings, obs_source_t *source);
+static void filter_destroy(void *data);
+static void filter_video_render(void *data, gs_effect_t *effect);
+
+struct filter {
+	obs_source_t *context;
+
+	gs_texrender_t *texrender_current_ptr;
+	gs_texrender_t *texrender_previous_ptr;
+
+	gs_texture_t *texture_shared_ptr;
+
+	uint32_t texture_shared_width;
+	uint32_t texture_shared_height;
+
+	ID3D11Texture2D *d3d11_shared_ptr;
+	ID3D11Texture2D *d3d11_current_ptr;
+	ID3D11Texture2D *d3d11_previous_ptr;
+
+	bool render_swap;
+	bool render_flush;
+
+	Microsoft::WRL::ComPtr<ID3D11DeviceContext> d3d11_context_ptr;
+};
+
+struct obs_source_info create_filter_info()
+{
+	struct obs_source_info filter_info = {};
+
+	filter_info.id = OBS_PLUGIN_;
+
+	filter_info.type = OBS_SOURCE_TYPE_FILTER;
+	filter_info.output_flags = OBS_SOURCE_VIDEO | OBS_SOURCE_SRGB;
+
+	filter_info.get_name = filter_get_name;
+
+	filter_info.get_properties = filter_properties;
+	filter_info.get_defaults = filter_defaults;
+
+	filter_info.create = filter_create;
+	filter_info.destroy = filter_destroy;
+
+	filter_info.video_render = filter_video_render;
+	filter_info.update = filter_update;
+
+	return filter_info;
+};
+
+} // namespace SharedTexture
